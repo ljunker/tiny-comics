4fb7f4c3e8483f38d43a450d977650f89c1c1d7e
Lars Junker <larsjunker92@gmail.com>
2025-08-20 13:41:03 +0200

    prune images after release

--- release.sh
+++ release.sh
@@ -6,4 +6,11 @@ k3d image import tiny-comics:latest -c njord-cluster
 kubectl rollout restart deploy/comics
 kubectl rollout status deploy/comics
 kubectl get pods -l app=comics -o jsonpath='{range .items[*]}{.metadata.name}{"  "}{.status.containerStatuses[0].imageID}{"\n"}{end}'
+docker ps --format '{{.Names}}' | grep k3d-
+
+# Prune dangling/unused images in all k3d nodes
+for node in $(docker ps --format '{{.Names}}' | grep k3d-); do
+    echo "Pruning images in $node..."
+    docker exec $node crictl rmi --prune || docker exec $node nerdctl image prune -f
+done
 
