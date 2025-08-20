a4f6affefc930a48da2033832c4acf396d21e1ad
Lars Junker <larsjunker92@gmail.com>
2025-08-20 13:37:23 +0200

    release steps

--- release.sh
+++ release.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 git pull
 docker build -t tiny-comics:latest .
+kubectl get pods -l app=comics -o jsonpath='{range .items[*]}{.metadata.name}{"  "}{.status.containerStatuses[0].imageID}{"\n"}{end}'
 k3d image import tiny-comics:latest -c njord-cluster
+kubectl rollout restart deploy/comics
+kubectl rollout status deploy/comics
+kubectl get pods -l app=comics -o jsonpath='{range .items[*]}{.metadata.name}{"  "}{.status.containerStatuses[0].imageID}{"\n"}{end}'
+
